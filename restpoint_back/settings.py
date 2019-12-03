e1b50dddbb213b835a4fa8dbab31d7bd286e8990
Marcos Castillo <marcoscastillo@MacBook-Pro-2.local>
2019-12-03 12:40:24 -0600

    setup CORS request access for deployed RestPoint app

--- restpoint_back/settings.py
+++ restpoint_back/settings.py
@@ -33,6 +33,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -113,3 +114,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 django_heroku.settings(locals())
+
+CORS_ORIGIN_WHITELIST = ('restpoint.herokuapp.com')
